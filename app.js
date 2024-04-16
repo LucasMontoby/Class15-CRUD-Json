87519101a90cc86742be0fc25c5df3154e34179a
LUCAS\monto <lucasmontoby@gmail.com>
2024-04-16 19:33:37 -0300

    Create

--- app.js
+++ app.js
@@ -2,16 +2,17 @@ const express = require ('express');
 const app = express();
 const path = require ('path');
 const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const routerProducto = require( './router/productoRouter' );
-app.use("/", routerProducto)
+app.use("/", routerProducto);
 
 app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
@@ -21,3 +22,4 @@ app.listen(3000, () => {
 app.get("/", (req, res) => {
     res.render(__dirname + "/views/home.ejs")
 })
+
--- router/productoRouter.js
+++ router/productoRouter.js
@@ -3,4 +3,7 @@ const productoController = require("../controller/productosController");
 
 router.get("/", productoController.list);
 
+router.get("/create", productoController.create);
+router.post("/create", productoController.stock);
+
 module.exports = router;
